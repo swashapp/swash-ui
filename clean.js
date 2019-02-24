972b8d3bafc1b7e96669c88a843cf4cb8ded166a
abdi <m.abdi@aut.ac.ir>
2019-02-24 11:23:21 +0330

    clean done

--- clean.js
+++ clean.js
@@ -13,3 +13,34 @@ for(var arg of process.argv){
     }
 }
 console.log("Src=", src,", Dst=",dst);
+
+
+var fs = require('fs');
+var p = require("path")
+
+var deleteFolderRecursive = function(path) {
+  if (fs.existsSync(path)) {
+    fs.readdirSync(path).forEach(function(file, index){
+      var curPath = p.join(path, file);
+      if (fs.lstatSync(curPath).isDirectory()) { // recurse
+        deleteFolderRecursive(curPath);
+      } else { // delete file
+        fs.unlinkSync(curPath);
+      }
+    });
+    fs.rmdirSync(path);
+  }
+};
+
+if (fs.existsSync(p.join(dst,"index.html"))){
+    fs.unlinkSync(p.join(dst,"index.html"));
+    console.log(p.join(dst,"index.html"), " deleted.");
+}else{
+    console.log(p.join(dst,"index.html"), " not found.");
+}
+if (fs.existsSync(p.join(dst,"static"))){
+    deleteFolderRecursive(p.join(dst,"static"));
+    console.log(p.join(dst,"static"), " folder deleted.")
+}else{
+    console.log(p.join(dst,"static"), " not found.");
+}
