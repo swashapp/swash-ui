20ff203e14a44e06f4614e2bdfe4dad6ede9bb36
mabdi <m.abdi@aut.ac.ir>
2019-09-16 00:59:52 +0200

    sidebar theme

--- src/components/sideNavigation.js
+++ src/components/sideNavigation.js
@@ -5,6 +5,9 @@ import { NavLink } from 'react-router-dom';
 import resource from "../resource";
 
 class SideNavigation extends React.Component {
+    
+
+
     render(){
     return <div className="sidebar-fixed position-fixed">
             <a href="#!" className="logo-wrapper waves-effect">
@@ -13,44 +16,37 @@ class SideNavigation extends React.Component {
             <MDBListGroup className="list-group-flush">
                 
                 <NavLink exact={true} to="/Profile" activeClassName="activeClass">
-                    <MDBListGroupItem>
-                        <MDBIcon icon="user-cog" className="mr-3"/>
+                    <MDBListGroupItem className="side-bar-text">
                         Profile
                     </MDBListGroupItem>
                 </NavLink>
                 <NavLink to="/Modules" activeClassName="activeClass">
-                    <MDBListGroupItem>
-                        <MDBIcon icon="plug" fas className="mr-3"/>
+                    <MDBListGroupItem className="side-bar-text">
                         Modules
                     </MDBListGroupItem>
                 </NavLink>
                 <NavLink to="/Filters" activeClassName="activeClass">
-                    <MDBListGroupItem>
-                        <MDBIcon fas icon="filter"  className="mr-3"/>
+                    <MDBListGroupItem className="side-bar-text">
                         
                         Advanced Filters
                     </MDBListGroupItem>
                 </NavLink>
                 <NavLink to="/Marketplace" activeClassName="activeClass">
-                    <MDBListGroupItem>
-                        <MDBIcon icon="cart-plus" fas className="mr-3"/>
+                    <MDBListGroupItem className="side-bar-text">
                         Marketplace
                     </MDBListGroupItem>
                 </NavLink><NavLink to="/Messages" activeClassName="activeClass">
-                    <MDBListGroupItem>
-                        <MDBIcon icon="envelope" fas className="mr-3"/>
+                    <MDBListGroupItem className="side-bar-text">
                         Messages
                     </MDBListGroupItem>
                 </NavLink>
                 <NavLink to="/Manual" activeClassName="activeClass">
-                    <MDBListGroupItem>
-                        <MDBIcon icon="book" fas className="mr-3"/>
+                    <MDBListGroupItem className="side-bar-text">
                         Manual
                     </MDBListGroupItem>
                 </NavLink>
                 <NavLink to="/About" activeClassName="activeClass">
-                    <MDBListGroupItem>
-                        <MDBIcon icon="question" fas className="mr-3"/>
+                    <MDBListGroupItem className="side-bar-text">
                         About
                     </MDBListGroupItem>
                 </NavLink>
