f63d5945312ff18cf366c02f78de3a0246977dc7
khalilzadeh <Khalilzadeh@KHALILZADEH.Behsazan.net>
2019-10-09 19:45:58 +0330

    review

--- src/components/microcomponents/CustomSelect.js
+++ src/components/microcomponents/CustomSelect.js
@@ -26,18 +26,20 @@ class CustomSelect extends React.Component {
   
   handleMenuClick(e,item) {    
     this.setState({selectedItem: item})
+    if(this.props.onChange)
+      this.props.onChange(item);
   }
 
   render() {
     return (
       <Dropdown className="swash-select" isOpen={this.state.dropdownOpen} toggle={this.toggle}>
-        <DropdownToggle className='swash-select-container' tag='div'>
+        <DropdownToggle className={this.props.className?this.props.className:'swash-select-container'} tag='div'>
             <div className="swash-select-default">{this.state.selectedItem.description}</div>            
             <div className="swash-select-arrow">
                 <img src={arrow}/>
             </div>
         </DropdownToggle>
-        <DropdownMenu className="swash-select-menu">
+        <DropdownMenu className={this.props.menuClassName?this.props.menuClassName:"swash-select-menu"}>
           {this.state.items.map((item) => { return <DropdownItem onClick={(e) => this.handleMenuClick(e,item)} className="swash-select-item" tag='div'>{item.description}</DropdownItem>
           })}                     
         </DropdownMenu>
--- src/components/microcomponents/ModuleDetailView.js
+++ src/components/microcomponents/ModuleDetailView.js
@@ -1,173 +1,195 @@
 import React from 'react';
 
 import CustomCheckBox from './CustomCheckBox';
+import CustomSelect from './CustomSelect';
 
 class ModuleDetailView extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          views: {},
-          group_selected: this.props.module.viewGroups[0].name
-        };
-    }
-
-    componentDidMount() {
-        if (this.props.module) {
-			let views = {};
-            let module = this.props.module;
-            if (module) {                
-				for(let view of module.viewGroups) {
-					views[view.name] = {
-						name: view.name,
-						title: view.title,
-						items: []
-						}
-				}
-				let functions = module.functions;
-				for(let func of functions) {
-					if(module[func]) {
-						let index = 0
-						for (let item of module[func]) {
-							views[item.viewGroup].items.push({
-								name: item.name,
-								title: item.title,
-								description: item.description,
-								is_enabled: item.is_enabled,
-								func: func,
-								index: index
-							})
-							index++;
-						}
-					}
-				}
-			}
-            
-/*			
-			if (module.apiCall) {			  
-				let f  = setInterval(()=>{window.helper.isConnected(module.name).then(connected => {
-					this.setState({connected:connected})
-				});},1000);
-				this.setState({intervalId: f});
-			}                
-*/
-			this.setState({
-				module: module,
-				is_enabled: module.is_enabled,								
-				connected: module.access_token?true:false,
-				views: views,
-			})			
-		}
+  constructor(props) {
+    super(props);
+    this.state = {
+      views: {},
+      connected: false,
+      group_selected: this.props.module.viewGroups[0].name
     };
+    this.onSelectChange = this.onSelectChange.bind(this);
+  }
 
-    componentDidUpdate(prevProps){
-    	
-    }
-
-    handleOauth(){
-
-    }
   
-  
-    selectAll(e, state){			
-        let views = this.state.views;    
-        for (let viewName in views) {
-            {
-                for (let itemId in views[viewName].items) {
-                    views[viewName].items[itemId].is_enabled = state;
-                }
-            }				
+
+  componentDidMount() {
+    if (this.props.module) {
+      let views = {};
+      let module = this.props.module;
+      if (module) {
+        for (let view of module.viewGroups) {
+          views[view.name] = {
+            name: view.name,
+            title: view.title,
+            items: []
+          }
         }
-        this.setState({views: views});				
+        let functions = module.functions;
+        for (let func of functions) {
+          if (module[func]) {
+            let index = 0
+            for (let item of module[func]) {
+              views[item.viewGroup].items.push({
+                name: item.name,
+                title: item.title,
+                description: item.description,
+                is_enabled: item.is_enabled,
+                func: func,
+                index: index
+              })
+              index++;
+            }
+          }
+        }
+      }
+
+      /*			
+            if (module.apiCall) {			  
+              let f  = setInterval(()=>{window.helper.isConnected(module.name).then(connected => {
+                this.setState({connected:connected})
+              });},1000);
+              this.setState({intervalId: f});
+            }                
+      */
+      this.setState({
+        module: module,
+        is_enabled: module.is_enabled,
+        connected: module.access_token ? true : false,
+        views: views,
+      })
     }
+  };
 
-      
+  componentDidUpdate(prevProps) {
 
-    getSettings(settings) {	
-		let views = this.state.views
-		for (let viewName in views) {
-			{
-				for (let itemId in views[viewName].items) {
-					let f = document.getElementById(viewName + "-" + itemId).checked;
-					settings[views[viewName].items[itemId].func][views[viewName].items[itemId].name] = f;
-				}
-			}				
-		}
-	}
+  }
+
+  connect(name){
+    window.helper.startAuth(name).then(x => {
+      let moduleName = this.state.name;                                               
+              this.setState({connected:'connecting'})				
+    });
+  }
+  
+  disconnect(name){
+    window.helper.removeAuth(name).then(x => {      
+      window.helper.isConnected(name).then(connected => {
+        this.setState({connected: connected});
+      });
+    });
+  }
 
 
 
-  getCollectors(){
-    if(! this.state.views)
+  selectAll(e, state) {
+    let views = this.state.views;
+    for (let viewName in views) {
+      {
+        for (let itemId in views[viewName].items) {
+          views[viewName].items[itemId].is_enabled = state;
+        }
+      }
+    }
+    this.setState({ views: views });
+  }
+
+
+
+  getSettings(settings) {
+    let views = this.state.views
+    for (let viewName in views) {
+      {
+        for (let itemId in views[viewName].items) {
+          let f = document.getElementById(viewName + "-" + itemId).checked;
+          settings[views[viewName].items[itemId].func][views[viewName].items[itemId].name] = f;
+        }
+      }
+    }
+  }
+
+
+  onSelectChange(item) {
+    this.setState({group_selected: item.value});
+  }
+
+  getCollectors() {
+    if (!this.state.views)
       return "";
-      const dropbox = (<div><div className="form-caption">Choose a search engine</div>
-                            <div>
-                        <select id='filterOption' 
-                        className="browser-default custom-select" 
-                        onChange={(e)=> this.setState({group_selected: e.target.value})}
-                        defaultValue={this.state.views[0]}
-                        >
-                          {this.props.module.viewGroups.map((ob,id)=>  <option value={ob.name}>{ob.title}</option>)}
-                        </select>
-                            </div></div>);
-      
-    return (<div>
-        {this.props.module.style ==='dropbox'?dropbox:''}
-        {this.state.views? Object.keys(this.state.views).map((key,index) =>
-          <>
-              {((this.props.module.style ==='dropbox' && this.state.group_selected === key) || (this.props.module.style !='dropbox'))?<>
-              <div className="module-detail-view-title-container">
-                <div className="module-detail-view-title">{this.state.views[key].title}</div>
-                {key == 'API'? <>                
-                    <a className="oauth_btn" onClick={()=> this.handleOauth()}>Connect to {this.props.module.name}</a>
-                    <a className="oauth_btn" onClick={()=> this.handleOauth()}>Connect to {this.props.module.name}</a>
-                </>:''}
-              </div>
-              <div className="checkbox-container">
-                {this.state.views[key].items.map( (collector, id)=> 
-                    <div className="module-detail-view-checkbox" >
-                        <label>{/*<input type="checkbox" value={data.name} />*/}
+    let selectItems =[];
+    this.props.module.viewGroups.map((ob, id) => selectItems.push({description: ob.title, value: ob.name}));
+    let searchEngings = (<div>
+      <CustomSelect items={selectItems} className={'search-select-container'} menuClassName='search-select-menu' onChange={this.onSelectChange} />
+    </div >);
 
-                            <CustomCheckBox  id={this.state.views[key].name + "-" + id} checked={collector.is_enabled}  handleClick={(x)=> {/*this.state.views[key].items[id].is_enabled = x*/} } />
-                            <div className="label">{collector.title}</div>                        
-                        </label>
-                    </div>           
-                )}
-              </div>
-              </>:''}
-          </>)
-        :''}
+    const dropbox = (<div style={{marginTop: '48px'}}><div className="form-caption">Choose a search engine</div>
+      <div>
+        {searchEngings}
+      </div></div>);
+
+    return (<div>
+      {this.props.module.style === 'dropbox' ? dropbox : ''}
+      {this.state.views ? Object.keys(this.state.views).map((key, index) =>
+        <>
+          {((this.props.module.style === 'dropbox' && this.state.group_selected === key) || (this.props.module.style != 'dropbox')) ? <>
+            <div className="module-detail-view-title-container">
+              <div className="module-detail-view-title">{this.state.views[key].title}</div>
+              {key == 'API' ? <>
+                {this.state.connected && this.state.connected != 'connecting'?
+                <a className="oauth_btn" onClick={() => this.disconnect(this.props.module.name)}>Disconnect</a>
+                :<a className="oauth_btn" onClick={() => this.connect(this.props.module.name)}>Connect to {this.props.module.name}</a>                
+                }
+              </> : ''}
+            </div>
+            <div className="checkbox-container">
+              {this.state.views[key].items.map((collector, id) =>
+                <div className="module-detail-view-checkbox" >
+                  <label>{/*<input type="checkbox" value={data.name} />*/}
+
+                    <CustomCheckBox id={this.state.views[key].name + "-" + id} checked={collector.is_enabled} handleClick={(x) => {/*this.state.views[key].items[id].is_enabled = x*/ }} />
+                    <div className="label">{collector.title}</div>
+                  </label>
+                </div>
+              )}
+            </div>
+          </> : ''}
+        </>)
+        : ''}
     </div>);
   }
 
-   
- 
-        
-        
 
 
-  
 
-  getButtons(){
-    return <div style={{height: 40, marginTop: 48}}>
-      <a className="module-btn " onClick={(e) => {this.selectAll(e, false)}} style={{width: 112, float:"left"}}>Deselect all</a>
-      <a className="module-btn " onClick={(e) => {this.selectAll(e, true)}} style={{width: 112, float:"left"}}>Select all</a>
-      <a className="module-btn " style={{width: 72, float:"right"}}>Done</a>
+
+
+
+
+
+  getButtons() {
+    return <div style={{ height: 40, marginTop: 32 }}>
+      <a className="module-btn " onClick={(e) => { this.selectAll(e, false) }} style={{ width: 112, float: "left" }}>Deselect all</a>
+      <a className="module-btn " onClick={(e) => { this.selectAll(e, true) }} style={{ width: 112, float: "left" }}>Select all</a>
+      <a className="module-btn " style={{ width: 72, float: "right" }}>Done</a>
     </div>
   }
-  
-  
-  render(){
-    const collectors = this.getCollectors();    
+
+
+  render() {
+    const collectors = this.getCollectors();
     const buttons = this.getButtons();
     return (
-          <>
-              <div className="module-detail-view-description">{this.props.module.description}</div>
-              {collectors}              
-              {buttons}
-          </>
-      );
+      <>
+        <div className="module-detail-view-description">{this.props.module.description}</div>
+        {collectors}
+        {buttons}
+      </>
+    );
   }
-  
+
 }
 
 export default ModuleDetailView;
